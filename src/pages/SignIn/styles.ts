e06372daa3fe9c48bc37749bc3b87c1cdf65acda
Michael Moura <mikemorcerf@gmail.com>
2020-04-26 22:24:04 -0400

    create Input and Button components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons';
 
 export const Container = styled.View`
 	flex: 1;
 	align-items: center;
 	justify-content: center;
+	padding: 0 30px;
 `;
 
 export const Title = styled.Text`
