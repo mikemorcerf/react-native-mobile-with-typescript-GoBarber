bb3b27cb0e0d5a9d27fe2c558e20b7200ba83ad7
Mike Morcerf <mikemorcerf@gmail.com>
2020-11-29 19:32:42 -0500

    update baseURL to test using a simulator

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://192.168.0.18:3333',
+	// commented out address if testing using physical device
+	// baseURL: 'http://192.168.0.18:3333',
+	baseURL: 'http://localhost:3333',
 });
 
 export default api;
